513627c17c562532774f150ea46cd02dcaae61ec
David Hurtado <david0219hg@gmail.com>
2022-12-21 17:08:25 +0000

    adding rest framework for the web

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'titles.apps.TitlesConfig'
+    'titles.apps.TitlesConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
